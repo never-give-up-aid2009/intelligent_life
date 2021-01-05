f5f7e76a46100a08e836f4cc8e131008013e4559
aid2009bzx <1070727987@qq.com>
2021-01-05 18:49:24 +0800

    add

--- Mall/order/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.order),
-    url(r'^order_handler/$', views.order_handler),
-    url(r'^pay/(\d+)$', views.pay)
-]
